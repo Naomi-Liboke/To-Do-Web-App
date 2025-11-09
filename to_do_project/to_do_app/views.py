d690d0207b51ef5e7067e9d4f71a2d3e55904ee5
Naomi-Liboke <libokenaomi@gmail.com>
2025-11-10 01:00:39 +0200

    to_do_app setup

--- to_do_project/to_do_app/views.py
+++ to_do_project/to_do_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
  
 def home(request):
-    return render(request, 'blog/home.html')
+    return render(request, 'to_do_app/home.html')
 
